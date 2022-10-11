013ade0d4f0e2b3f0a8fcf344b2531ed555c42db
Joe Atteen <joeyatteen@gmail.com>
2022-10-11 18:31:16 +0000

    form validation added

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function AddTransaction({ AddNewTransaction, id }) {
   const [option, setOption] = useState("");
   const [item, setItem] = useState("");
   const [date, setDate] = useState("");
   const [amount, setAmount] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const Submit = (e) => {
-    e.preventDefault();
+  const Submit = (data) => {
     let transactionInfo = { option, item, date, amount, id: id };
     AddNewTransaction(transactionInfo);
     setOption("");
     setItem("");
     setDate("");
     setAmount("");
+    console.log(data);
   };
 
   return (
@@ -29,6 +35,7 @@ function AddTransaction({ AddNewTransaction, id }) {
           title="select category"
           className="category"
           value={option}
+          {...register("cat", { required: true })}
           onChange={(e) => {
             setOption(e.target.value);
           }}
@@ -46,30 +53,38 @@ function AddTransaction({ AddNewTransaction, id }) {
           type="text"
           placeholder="Enter item/service..."
           value={item}
+          {...register("service", { required: true })}
           onChange={(e) => {
             setItem(e.target.value);
           }}
         />
+        {errors.service && (
+          <p className="warning">This field must be filled!</p>
+        )}
         <br />
         <input
           type="date"
           placeholder="Select date"
           value={date}
+          {...register("dat", { required: true })}
           onChange={(e) => {
             setDate(e.target.value);
           }}
         />
+        {errors.dat && <p className="warning">This field must be filled!</p>}
         <br />
         <input
           type="number"
-          placeholder="Enter amount"
+          placeholder="Enter amount (precede it with a '-' if it's an expense)"
           value={amount}
+          {...register("number", { required: true })}
           onChange={(e) => {
             setAmount(e.target.value);
           }}
         />
+        {errors.number && <p className="warning">This field must be filled!</p>}
       </form>
-      <button type="button" className="btn" onClick={Submit}>
+      <button type="button" className="btn" onClick={handleSubmit(Submit)}>
         Add Transaction
       </button>
     </div>
